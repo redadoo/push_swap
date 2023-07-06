8f5af31e88c8b61f1554ff44aca17ecfa39e8a77
edoardo <edoardovocaturo@gmail.com>
2023-07-06 20:53:27 +0200

    change range for python random gen

--- stack_gen/stack_gen.py
+++ stack_gen/stack_gen.py
@@ -3,8 +3,8 @@ import sys
 
 stack = []
 while (len(stack) < int(sys.argv[1])):
-        #stack.append(random.randint(-2147483646,2147483646))
-        stack.append(random.randint(0,100))
+        stack.append(random.randint(-2147483646,2147483646))
+        #stack.append(random.randint(0,100))
         stack = list(set(stack))
 
 if sorted(stack) == stack:
